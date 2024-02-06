4584ee113be5b5da7d91bed59f7364d23344553e
CrisPerez0206 <cristopher240871@gmail.com>
2024-02-06 11:10:26 -0500

    ..

--- ludobraintest_v2_0_0/server/db.js
+++ ludobraintest_v2_0_0/server/db.js
@@ -8,9 +8,9 @@ const mysql = require('mysql');
 const db = mysql.createPool({
     connectionLimit: 25,
     host: '3.134.64.181',
-    user: USER,
-    password: PASSWORD,
-    database: DATABASE
+    user: 'root',
+    password: 'Cr1stopherp3rez',
+    database: 'bdd_ludotest'
 })
 
 module.exports = db;

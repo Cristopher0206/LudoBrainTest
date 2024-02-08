ad1a18ea6e025368f65db853d5efbf6cbe57e251
CrisPerez0206 <cristopher240871@gmail.com>
2024-02-08 00:02:53 -0500

    .

--- ludobraintest_v2_0_0/pages/index.js
+++ ludobraintest_v2_0_0/pages/index.js
@@ -1,11 +1,12 @@
 import {Inter} from 'next/font/google'
 import Login from "@/pages/login";
+import styles from "@/styles/styles.module.css";
 
 const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
     return (
-        <main className={`bg-amber-50 min-h-screen`}>
+        <main className={`min-h-screen ${styles.bg_robot_image}`}>
             <Login></Login>
         </main>
     )
